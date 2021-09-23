1f7e99d421c944e0cb7f849e1fed856f05bd3dfd
Hamam <zero.saturnus@gmail.com>
2021-09-23 23:43:04 +0700

    add text in splash screen

--- src/Containers/Splash/index.js
+++ src/Containers/Splash/index.js
@@ -1,22 +1,30 @@
 import React from 'react';
-import { View, ImageBackground, StyleSheet } from 'react-native';
+import { View, ImageBackground, StyleSheet, Text } from 'react-native';
 import { SplashImage } from '../../Assets';
 
 const Splash = () => {
   return (
-    <View style={styles.container}>
-      <ImageBackground
-        source={SplashImage}
-        resizeMode={'cover'}
-        style={styles.container}
-      />
-    </View>
+    <ImageBackground
+      source={SplashImage}
+      resizeMode={'cover'}
+      style={styles.container}>
+      <Text style={styles.title}>{'WELCOME TO MONUMENTAL HABITS'}</Text>
+    </ImageBackground>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    alignItems: 'center',
+  },
+  title: {
+    fontFamily: 'Klasik-Regular',
+    fontSize: 40,
+    color: '#573353',
+    textAlign: 'center',
+    width: 260,
+    marginTop: 96,
   },
 });
 
