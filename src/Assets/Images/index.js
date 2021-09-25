306540f331ab34be4e16c709900a06418cfc292b
Hamam <zero.saturnus@gmail.com>
2021-09-25 21:14:14 +0700

    develop : export image

--- src/Assets/Images/index.js
+++ src/Assets/Images/index.js
@@ -1,2 +1,13 @@
 import SplashImage from './splash.jpg';
-export { SplashImage };
+import IntroductionSlider1 from './introduction-slider-1.png';
+import IntroductionSlider2 from './introduction-slider-2.png';
+import IntroductionSlider3 from './introduction-slider-3.png';
+import IntroductionSlider4 from './introduction-slider-4.png';
+
+export {
+  SplashImage,
+  IntroductionSlider1,
+  IntroductionSlider2,
+  IntroductionSlider3,
+  IntroductionSlider4,
+};
